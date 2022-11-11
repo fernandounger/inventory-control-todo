286ee9c2d019d04086eb9f680a3ee9bb8e388613
Fernando Unger <fdasilvaunger@gmail.com>
2022-11-11 19:51:13 -0300

    chore: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './todo/create/create.component';
+import { ListComponent } from './todo/list/list.component';
+import { UpdateComponent } from './todo/update/update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todo/list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'todo/create',
+    component: CreateComponent
+  },
+  {
+    path: 'todo/update/:id',
+    component: UpdateComponent
+  },
+  {
+    path: 'todo/list',
+    component: ListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './todo/create/create.component';
+import { ListComponent } from './todo/list/list.component';
+import { UpdateComponent } from './todo/update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    ListComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
